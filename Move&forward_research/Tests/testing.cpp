649da81f3a0daba64fecff797d4725f4ba60aa47
Xenia <heiduk.k.k.s@yandex.ru>
2022-03-18 18:03:37 +0300

    Fixed readme

--- Move&forward_research/Tests/testing.cpp
+++ Move&forward_research/Tests/testing.cpp
@@ -9,19 +9,18 @@
 #define BEGIN_ANY_FUNC Function_name_sender sender(__FUNCTION__); 
 
 template<class T>
-void example_1(T arg) {
+void example(T&& arg) {
 	BEGIN_ANY_FUNC
-	volatile auto new_tmp = arg;
+	volatile auto new_tmp = my_move(arg); // my_move(arg)
 }
 
 template<class T>
 void wrapper(T&& arg) {
 	BEGIN_ANY_FUNC
-	printf("a = %d\n", arg.get_value());
-	example_1(arg);
-	printf("a = %d\n", arg.get_value());
+	example(arg);
 }
 
 void testing() {
-	wrapper(Int(10, "a"));
-} 
+	VAR(Int, a, 10);
+	wrapper(a);
+}
